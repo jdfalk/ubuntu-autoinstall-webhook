e970acbe4872d456776ecd0543d8a4a233cf6357
Johnathan Falk <johnathan.falk@gmail.com>
2025-02-22 10:31:12 -0500

    Add placeholder tests for database models, IPXE handler, utility functions, main function, and assets loading
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

---
+++ viewer-app/assets_test.go
@@ -0,0 +1,12 @@
+package assets
+
+import (
+	"testing"
+)
+
+func TestAssetsLoading(t *testing.T) {
+	t.Run("Placeholder", func(t *testing.T) {
+		// TODO: Implement assets loading tests
+		t.Log("Assets loading test needs implementation")
+	})
+}
