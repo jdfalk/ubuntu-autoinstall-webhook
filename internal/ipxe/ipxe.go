56d190a43182662ed6edc80746dd4dcddb345b3e
Johnathan Falk <johnathan.falk@gmail.com>
2025-02-22 21:47:33 -0500

    Add file history management for iPXE configurations; implement storeFileHistory function and corresponding tests
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- internal/ipxe/ipxe.go
+++ internal/ipxe/ipxe.go
@@ -2,12 +2,37 @@ package ipxe
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/spf13/viper"
 )
+
+// IPXEHistory stores the last five IPXE configuration versions per client.
+type IPXEHistory struct {
+	ID        string    `json:"id"`
+	ClientID  string    `json:"client_id"`
+	Config    string    `json:"config"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// storeFileHistory saves the current version of the iPXE file before modification.
+func storeFileHistory(macAddress, ipxeFilePath string) error {
+	content, err := ioutil.ReadFile(ipxeFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read iPXE file: %w", err)
+	}
+
+	historyFile := fmt.Sprintf("%s.history.%d", ipxeFilePath, time.Now().Unix())
+	err = ioutil.WriteFile(historyFile, content, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to store iPXE history: %w", err)
+	}
+
+	return nil
+}
 
 // UpdateIPXEFile modifies an iPXE file when a client reaches 25% progress
 func UpdateIPXEFile(macAddress string) error {
@@ -32,20 +57,18 @@ func UpdateIPXEFile(macAddress string) error {
 		return fmt.Errorf("no iPXE file found for MAC: %s", macAddress)
 	}
 
+	// Store file history before modification
+	err := storeFileHistory(macAddress, ipxeFilePath)
+	if err != nil {
+		return err
+	}
+
 	// Update the iPXE file to instruct normal boot
 	newContent := "#!ipxe\nexit\n"
-	err := os.WriteFile(ipxeFilePath, []byte(newContent), 0644)
+	err = os.WriteFile(ipxeFilePath, []byte(newContent), 0644)
 	if err != nil {
 		return fmt.Errorf("failed to update iPXE file: %w", err)
 	}
 
 	return nil
-}
-
-// IPXEHistory stores the last five IPXE configuration versions per client.
-type IPXEHistory struct {
-	ID        string    `json:"id"`
-	ClientID  string    `json:"client_id"`
-	Config    string    `json:"config"`
-	CreatedAt time.Time `json:"created_at"`
 }
