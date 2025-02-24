a1dce41d60d5e5fb3989ec7ba6f474df314e45f1
Johnathan Falk <johnathan.falk@gmail.com>
2025-02-23 23:31:35 -0500

    Disable lint, assets.go has to run two generate commands.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- viewer-app/assets.go
+++ viewer-app/assets.go
@@ -5,13 +5,10 @@ import (
 	"io/fs"
 )
 
+//go:generate npm install
 //go:generate npm run build
 //go:embed dist/viewer-app/browser/*
 var Assets embed.FS
 
 // AssetsFS is the embedded filesystem with the prefix stripped.
 var AssetsFS, _ = fs.Sub(Assets, "dist/viewer-app/browser")
-
-// DummyExport is a dummy exported variable to ensure export data is generated.
-// This variable serves no functional purpose.
-var DummyExport = true
