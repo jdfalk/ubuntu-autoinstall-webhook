888f9646ab61786acbbeeb9e4bc4f262067db1d3
Johnathan Falk <johnathan.falk@gmail.com>
2025-02-23 23:22:41 -0500

    dummyExport.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- viewer-app/assets.go
+++ viewer-app/assets.go
@@ -1,11 +1,17 @@
 package assets
 
-import "embed"
-import "io/fs"
+import (
+	"embed"
+	"io/fs"
+)
 
 //go:generate npm run build
 //go:embed dist/viewer-app/browser/*
 var Assets embed.FS
 
-// AssetsFS is the embedded filesystem with the prefix stripped
+// AssetsFS is the embedded filesystem with the prefix stripped.
 var AssetsFS, _ = fs.Sub(Assets, "dist/viewer-app/browser")
+
+// DummyExport is a dummy exported variable to ensure export data is generated.
+// This variable serves no functional purpose.
+var DummyExport = true
