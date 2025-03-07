f552f1a731cf11f17360b0df80dea135e5760557
Johnathan Falk <johnathan.falk@gmail.com>
2025-03-06 21:07:28 -0500

    changes to fix for mockery.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- internal/webserver/grpc_server.go
+++ internal/webserver/grpc_server.go
@@ -7,29 +7,28 @@ import (
 	"net"
 
 	pb "github.com/jdfalk/ubuntu-autoinstall-webhook/pkg/proto"
-
 	"google.golang.org/grpc"
 )
 
-// grpcServer implements the InstallService defined in the proto.
+// grpcServer implements the InstallServiceServer interface.
 type grpcServer struct {
 	pb.UnimplementedInstallServiceServer
 }
 
-// ReportStatus receives a status update from the client.
+// ReportStatus handles the ReportStatus RPC.
 func (s *grpcServer) ReportStatus(ctx context.Context, req *pb.StatusRequest) (*pb.StatusResponse, error) {
 	fmt.Printf("Received status update from %s: %d%% - %s\n", req.Hostname, req.Progress, req.Message)
 	return &pb.StatusResponse{Acknowledged: true}, nil
 }
 
-// StartGRPCServer starts a gRPC server on the given port.
+// StartGRPCServer starts the gRPC server on the specified port.
 func StartGRPCServer(port string) error {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		return fmt.Errorf("failed to listen on %s: %w", port, err)
 	}
-	grpcServer := grpc.NewServer()
-	pb.RegisterInstallServiceServer(grpcServer, &grpcServer{})
+	srv := grpc.NewServer()
+	pb.RegisterInstallServiceServer(srv, &grpcServer{})
 	fmt.Println("gRPC server is listening on", port)
-	return grpcServer.Serve(lis)
+	return srv.Serve(lis)
 }
