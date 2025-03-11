b84eaa1078526a9cbe5defa570b97045b243ce2e
Johnathan Falk <johnathan.falk@gmail.com>
2025-03-11 10:01:38 -0400

    Commit.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- web/angular/autoinstall-web/src/app/services/install.service.ts
+++ web/angular/autoinstall-web/src/app/services/install.service.ts
@@ -1,4 +1,3 @@
-// src/app/services/install.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -18,13 +17,11 @@ export interface StatusResponse {
     providedIn: 'root'
 })
 export class InstallService {
-    // Adjust the URL if needed (e.g., include hostname or port).
     private baseUrl = 'http://localhost:8080/v1/install';
 
     constructor(private http: HttpClient) { }
 
     reportStatus(status: StatusRequest): Observable<StatusResponse> {
-        // POST to the /status endpoint as defined in your proto HTTP annotations.
         return this.http.post<StatusResponse>(`${this.baseUrl}/status`, status);
     }
 }
--- web/angular/autoinstall-web/src/main.ts
+++ web/angular/autoinstall-web/src/main.ts
@@ -1,8 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app-routing.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)]
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
